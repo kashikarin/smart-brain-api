a7c928da636cf7e7d2c0d17df1771e10d6d381a5
kashikarin <164936604+kashikarin@users.noreply.github.com>
2024-12-10 17:40:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -47,10 +47,8 @@ app.put('/image', (req, res) => image.handleImage(req, res, db));
 
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
-const DATABASE_URL = process.env.DATABASE_URL;
-
 app.listen(3000, () => {
-    console.log(`app is running on port ${DATABASE_URL}`);
+    console.log('app is running on port 3000');
 });
     
 
